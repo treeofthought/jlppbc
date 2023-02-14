e0b39db9d5e5051dec551ee8749d2109d3ed601b
Stuart Mason <stuart@sunboundhomes.com>
2023-02-14 13:08:12 -0500

    P

--- src/App.js
+++ src/App.js
@@ -21,27 +21,31 @@ function App() {
       <div className="card">
       <h2>Best and Worst</h2>
       <h3>Top 5 Books with 2+ Ratings</h3>
-      <table><tr><th>Title</th>
-      <th>Author</th>
-      <th>Avg</th>
-      <th>J</th>
-      <th>S</th></tr>
-      <tr><td>The Amazing Adventures of Kavalier and Clay</td><td>Michael Chabon</td><td>9.3</td><td>9.4</td><td>9.2</td></tr>
-      <tr><td>The Remains of the Day</td><td>Kazuo Ishiguro</td><td>9.1</td><td>9.1</td><td>9</td></tr>
-      <tr><td>Hyperion</td><td>Dan Simmons</td><td>8.9</td><td>8.9</td><td>8.9</td></tr>
-      <tr><td>Red Rising</td><td>Pierce Brown</td><td>8.5</td><td>8.6</td><td>8.4</td></tr>
-      <tr><td>Golden Son</td><td>Pierce Brown</td><td>8.3</td><td>8.5</td><td>8.2</td></tr></table>
-      <h3>Bottom 5 Books with 2+ Ratings</h3>
-      <table><tr><th>Title</th>
+<table><tr><th>Title</th>
 <th>Author</th>
 <th>Avg</th>
 <th>J</th>
+<th>P</th>
 <th>S</th></tr>
-<tr><td>The Peacemaker's Code</td><td>Deepak Mahotra</td><td>1</td><td>0</td><td>2</td></tr>
-<tr><td>The Ruin of Kings</td><td>Jenn Lyons</td><td>2.5</td><td>1</td><td>4.1</td></tr>
-<tr><td>A Divided Spy</td><td>Charles Cumming</td><td>3.9</td><td>1</td><td>6.7</td></tr>
-<tr><td>The Dragon Reborn</td><td>Robert Jordan</td><td>7.5</td><td>7.8</td><td>7.2</td></tr>
-<tr><td>Royal Assassin</td><td>Robin Hobb</td><td>7.6</td><td>7.5</td><td>7.6</td></tr></table>
+<tr><td>Hyperion</td><td>Dan Simmons</td><td>9.1</td><td>8.9</td><td>9.4</td><td>8.9</td></tr>
+<tr><td>The Amazing Adventures of Kavalier and Clay</td><td>Michael Chabon</td><td>9.1</td><td>9.4</td><td>8.75</td><td>9.2</td></tr>
+<tr><td>The Remains of the Day</td><td>Kazuo Ishiguro</td><td>9.1</td><td>9.1</td><td>--</td><td>9</td></tr>
+<tr><td>Red Rising</td><td>Pierce Brown</td><td>9</td><td>8.6</td><td>10</td><td>8.4</td></tr>
+<tr><td>Neverwhere</td><td>Neil Gaiman</td><td>8.9</td><td>--</td><td>9.8</td><td>7.9</td></tr></table>
+
+
+      <h3>Bottom 5 Books with 2+ Ratings</h3>
+<table><tr><th>Title</th>
+<th>Author</th>
+<th>Avg</th>
+<th>J</th>
+<th>P</th>
+<th>S</th></tr>
+<tr><td>The Peacemaker's Code</td><td>Deepak Mahotra</td><td>0.7</td><td>0</td><td>0</td><td>2</td></tr>
+<tr><td>The Ruin of Kings</td><td>Jenn Lyons</td><td>1.7</td><td>1</td><td>0</td><td>4.1</td></tr>
+<tr><td>Artemis</td><td>Andy Weir</td><td>2</td><td>--</td><td>0</td><td>3.9</td></tr>
+<tr><td>Lethal White</td><td>Robert Galbraith</td><td>2.6</td><td>--</td><td>2</td><td>3.2</td></tr>
+<tr><td>A Divided Spy</td><td>Charles Cumming</td><td>2.7</td><td>1</td><td>0.5</td><td>6.7</td></tr></table>
 </div>
     </div>
   );
